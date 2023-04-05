8a0a3797954b3c2ce86d99a18d42b0d9ab7d43bb
Amal C P <acp070146@gmail.com>
2023-04-05 12:22:16 +0530

    team page edited

--- src/components/team/team.jsx
+++ src/components/team/team.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import "./team.css";
 import headTeam from "../../assets/team-header-img.svg";
 import enric from "../../assets/enric.svg";
+import talksession from "../../assets/talksession.svg";
+import expo from "../../assets/expo.svg";
+import handsonworkshop from "../../assets/handsonworkshop.svg";
+import games from "../../assets/games.svg";
+import quizes from "../../assets/quizes.svg";
+
 const team = () => {
   return (
     <section id="team">
@@ -14,93 +20,107 @@ const team = () => {
           <div class="box">
             <img src={enric} alt="" />
             <div class="name-tag-team">
-              <div class="name-team">Enric S Neelamkavil</div>
-              <div class="buttons-team">
-                <button>Button 1</button>
-                <button>Button 2</button>
+              <div class="name-team">
+                Enric S Neelamkavil <br /> Lead
               </div>
             </div>
           </div>
           <div class="box">
             <img src={enric} alt="" />
             <div class="name-tag-team">
-              <div class="name-team">Enric S Neelamkavil</div>
-              <div class="buttons-team">
-                <button>Button 1</button>
-                <button>Button 2</button>
+              <div class="name-team">
+                Enric S Neelamkavil <br /> Lead
               </div>
             </div>
           </div>
           <div class="box">
             <img src={enric} alt="" />
             <div class="name-tag-team">
-              <div class="name-team">Enric S Neelamkavil</div>
-              <div class="buttons-team">
-                <button>Button 1</button>
-                <button>Button 2</button>
+              <div class="name-team">
+                Enric S Neelamkavil <br /> Lead
               </div>
             </div>
           </div>
           <div class="box">
             <img src={enric} alt="" />
             <div class="name-tag-team">
-              <div class="name-team">Enric S Neelamkavil</div>
-              <div class="buttons-team">
-                <button>Button 1</button>
-                <button>Button 2</button>
+              <div class="name-team">
+                Enric S Neelamkavil <br /> Lead
               </div>
             </div>
           </div>
           <div class="box">
             <img src={enric} alt="" />
             <div class="name-tag-team">
-              <div class="name-team">Enric S Neelamkavil</div>
-              <div class="buttons-team">
-                <button>Button 1</button>
-                <button>Button 2</button>
+              <div class="name-team">
+                Enric S Neelamkavil <br /> Lead
               </div>
             </div>
           </div>
           <div class="box">
             <img src={enric} alt="" />
             <div class="name-tag-team">
-              <div class="name-team">Enric S Neelamkavil</div>
-              <div class="buttons-team">
-                <button>Button 1</button>
-                <button>Button 2</button>
+              <div class="name-team">
+                Enric S Neelamkavil <br /> Lead
+              </div>
+            </div>
+          </div>
+          <div class="box">
+            <img src={enric} alt="" />
+            <div class="name-tag-team">
+              <div class="name-team">
+                Enric S Neelamkavil <br /> Lead
+              </div>
+            </div>
+          </div>
+          <div class="box">
+            <img src={enric} alt="" />
+            <div class="name-tag-team">
+              <div class="name-team">
+                Enric S Neelamkavil <br /> Lead
+              </div>
+            </div>
+          </div>
+          <div class="box">
+            <img src={enric} alt="" />
+            <div class="name-tag-team">
+              <div class="name-team">
+                Enric S Neelamkavil <br /> Lead
               </div>
             </div>
           </div>
         </div>
         <div className="cd-team-wrapper">
           <div className="cd-left-team">
-            <p>WE CREATE AND DESIGN.</p>
-            <p>
+            <p className="cd-left-team-one">
+              WE <span className="text-color">CREATE</span> AND DESIGN.
+            </p>
+            <p className="cd-left-team-two">
               Vorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
               turpis molestie.
             </p>
           </div>
           <div className="cd-right-team">
-            <div className="right-cd-div">
-              <img src="" alt="" />
+            <a className="right-cd-div">
+              <img src={talksession} alt="" />
               <p>TALK SESSIONS</p>
-            </div>
-            <div className="right-cd-div">
-              <img src="" alt="" />
+            </a>
+            <a className="right-cd-div">
+              <img src={expo} alt="" />
               <p>EXPO</p>
-            </div>
-            <div className="right-cd-div">
-              <img src="" alt="" />
+            </a>
+            <a className="right-cd-div">
+              <img src={handsonworkshop} alt="" />
               <p>HANDS-ON WORKSHOPS</p>
-            </div>
-            <div className="right-cd-div">
-              <img src="" alt="" />
+            </a>
+            <a className="right-cd-div">
+              <img src={games} alt="" />
               <p>GAMES</p>
-            </div>
-            <div className="right-cd-div">
-              <img src="" alt="" />
+            </a>
+            <a className="right-cd-div">
+              <img src={quizes} alt="" />
               <p>QUIZES</p>
-            </div>
+            </a>
           </div>
         </div>
       </div>
