2f69c49839777c3415b4bec91e4d0cd1358a86dd
enric <enricsneelamkavil@gmail.com>
2023-04-11 15:57:24 +0530

    initaial setup of spiral lines

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -15,20 +15,81 @@ const Home = () => {
     script.async = true;
     script.defer = true;
     document.body.appendChild(script);
+    const spiralAnimationScript = document.createElement("script");
+    spiralAnimationScript.src = "/static/js/spiral.js";
+    document.body.appendChild(spiralAnimationScript);
+
+    document.querySelectorAll('.effect-letter').forEach(button => {
+
+      let div = document.createElement('div'),
+        letters = button.textContent.trim().split('');
+
+      function elements(letter, index, array) {
+
+        let element = document.createElement('span'),
+          part = (index >= array.length / 2) ? -1 : 1,
+          position = (index >= array.length / 2) ? array.length / 2 - index + (array.length / 2 - 1) : index,
+          move = position / (array.length / 2),
+          rotate = 1 - move;
+
+        element.innerHTML = !letter.trim() ? '&nbsp;' : letter;
+        element.style.setProperty('--move', move);
+        element.style.setProperty('--rotate', rotate);
+        element.style.setProperty('--part', part);
+
+        div.appendChild(element);
+
+      }
+
+      letters.forEach(elements);
+
+      button.innerHTML = div.outerHTML;
+
+      button.addEventListener('mouseenter', e => {
+        if (!button.classList.contains('out')) {
+          button.classList.add('in');
+        }
+      });
+
+      button.addEventListener('mouseleave', e => {
+        if (button.classList.contains('in')) {
+          button.classList.add('out');
+          setTimeout(() => button.classList.remove('in', 'out'), 950);
+        }
+      });
+
+    });
     return () => {
       document.body.removeChild(script);
+      // document.body.removeChild(spiralAnimationScript);
     };
   }, []);
   return (
     <div className="body-wrapper">
+
       <Navbar />
       <Header />
-      <About />
-      <Events />
-      <Team />
-      <Sponsors />
-      <Contacts />
-      <CopyRight />
+      <div className="spiral-container">
+        <div className="styles-test">
+          <section className="" id="">
+            <canvas id="canvas"></canvas>
+            <div className="dust-animation">
+              <div className="bg-animation">
+                <div id='stars2' className="dust"></div>
+                <div id='stars3' className="dust"></div>
+              </div>
+            </div>
+          </section>
+        </div>
+        <div className="spiral-innercontainer">
+        <About />
+        <Events />
+        <Team />
+        <Sponsors />
+        <Contacts />
+        <CopyRight />
+        </div>
+      </div>
     </div>
   );
 };
