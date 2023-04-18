40eab48f3064e672b48923feb1f95447b013919a
Amal C P <acp070146@gmail.com>
2023-04-18 12:15:15 +0530

    added firebase storage

--- src/pages/designathon/Designathons.jsx
+++ src/pages/designathon/Designathons.jsx
@@ -7,7 +7,7 @@ import Sponsors from "../../components/sponsors/sponsors";
 // import header_video from "../../assets/designathon/Designathon bg video.mp4";
 import theme_bg from "../../assets/designathon/designathon-theme-bg.png";
 import Timeline from "./timeline";
-import human_gif from '../../assets/designathon/Designathon bg video 1.gif'
+import human_gif from "../../assets/designathon/Designathon bg video 1.gif";
 
 const Designathon = () => {
   React.useEffect(() => {
@@ -77,7 +77,7 @@ const Designathon = () => {
       }}
     >
       <Navbar />
-      <div style={{ position: "relative",background:'black'}}>
+      <div style={{ position: "relative", background: "black" }}>
         <div className="spiral-container">
           <div className="styles-test">
             <section className="" id="">
@@ -137,7 +137,10 @@ const Designathon = () => {
                 {/* <video loop autoPlay>
                   <source src={header_video} type="video/mp4" />
                 </video> */}
-                <img src={human_gif} alt="" />
+                <img
+                  src={`https://firebasestorage.googleapis.com/v0/b/cdw2k23.appspot.com/o/Designathon%20bg%20video%201.gif?alt=media&token=ef00d745-f5c0-4efb-ba32-90da3248102e`}
+                  alt=""
+                />
               </div>
             </div>
             <div className="designathon-theme image-container">
