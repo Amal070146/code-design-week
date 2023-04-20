2321927913cbb2c62509ec5924dacc7f3310b92a
Amal C P <acp070146@gmail.com>
2023-04-20 11:21:08 +0530

    timeline edited

--- src/pages/designathon/timeline.jsx
+++ src/pages/designathon/timeline.jsx
@@ -10,53 +10,62 @@ const timeline = () => {
           isLeft={true}
           date={"2023-04-18"}
           title={"REGISTRATION STARTS"}
+          days={"18 APR 2023"}
         />
         <TimelineSingle
           isLeft={false}
           date={"2023-05-18"}
           title={"REGISTRATION ENDS"}
+          days={"18 MAY 2023"}
         />
         <TimelineSingle
           isLeft={true}
           date={"2023-05-20"}
           title={"SUBMISSION OF IDEAS"}
+          days={"20 MAY 2023"}
         />
         <TimelineSingle
           isLeft={false}
-          date={"2023-05-25"}
-          title={"SHORTLISTED TEAMS ARE ANNOUNCED"}
+          date={"2023-05-22"}
+          title={"CODe DESIGN WEEK BEGINS"}
+          days={"22 MAY 2023"}
         />
         <TimelineSingle
           isLeft={true}
-          date={"2023-05-22"}
-          title={"CODe DESIGN WEEK BEGINS"}
+          date={"2023-05-25"}
+          title={"SHORTLISTED TEAMS ARE ANNOUNCED"}
+          days={"25 MAY 2023"}
         />
         <TimelineSingle
           isLeft={false}
           date={"2023-05-27"}
           title={"INAUGURATION OF DESIGNATHON"}
+          days={"27 MAY 2023"}
         />
         <TimelineSingle
           isLeft={true}
           date={"2023-05-27"}
           title={"DESIGNATHON BEGINS"}
+          days={"27 MAY 2023"}
         />
         <TimelineSingle
           isLeft={false}
           date={"2023-05-28"}
           title={"DESIGNATHON ENDS"}
+          days={"28 MAY 2023"}
         />
         <TimelineSingle
           isLeft={true}
           date={"2023-05-28"}
           title={"VALEDECTORY CEREMONY OF CDW ‘23"}
+          days={"28 MAY 2023"}
         />
       </div>
     </div>
   );
 };
 
-const TimelineSingle = ({ isLeft, date ,title}) => {
+const TimelineSingle = ({ isLeft, date ,title,days}) => {
   const [isCompleted, setIsCompleted] = useState(false);
   useEffect(() => {
     console.log(date);
@@ -74,16 +83,16 @@ const TimelineSingle = ({ isLeft, date ,title}) => {
         className="timelinesingleContentWrapper"
         style={{
           textAlign: isLeft ? "right" : "left",
-          borderLeft: isLeft ? null : "3px solid white",
+          borderLeft: isLeft ? null : "4px solid white",
           borderColor: isCompleted ? "#E30069" : "white",
-          borderWidth: "3px",
-          borderRight: isLeft ? "3px solid white" : null,
-          translate: isLeft ? "1.2px" : "-1.2px",
+          borderWidth: "4px",
+          borderRight: isLeft ? "4px solid white" : null,
+          translate: isLeft ? "2.2px" : "-2px",
         }}
       >
         {isCompleted && (
           <div
-            className="round"                                              
+            className="round"
             style={{
               left: !isLeft ? "calc(-10px - 1.5px)" : "",
               right: isLeft ? "calc(-10px - 1.5px)" : "",
@@ -92,7 +101,8 @@ const TimelineSingle = ({ isLeft, date ,title}) => {
             <div></div>
           </div>
         )}
-        <span>{date}</span>
+        <span style={{display:"none"}}>{date}</span>
+        <span>{days}</span>
         <h2>{title}</h2>
       </div>
       <div></div>
